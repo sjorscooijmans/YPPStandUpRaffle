4c57ad201c1ad375431aee3bb78ac1086f900e54
Sjors Cooijmans <sjors.cooijmans@hotmail.com>
2023-02-10 14:58:31 +0100

    updates

--- YppMachine/src/app/machine/machine.page.ts
+++ YppMachine/src/app/machine/machine.page.ts
@@ -1,16 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  keyframes
+} from '@angular/animations';
+
+
 
 @Component({
   selector: 'app-machine',
   templateUrl: './machine.page.html',
   styleUrls: ['./machine.page.scss'],
+  animations: [
+    trigger('rotating', [
+      state('begin', style({
+      })),
+      state('end', style({
+        transform: "translateY(100vw)"
+      })),
+      transition('begin => end', [
+        animate('1s', keyframes([
+          style({ transform: 'translateY(100vw)' }),
+        ]))
+      ]),
+    ])
+  ]
 })
+
 export class MachinePage implements OnInit {
+  start = true
 
   personen = [
-    {name: 'sjors', image: ''},
-    {name: 'martijn', image: ''},
-    {name: 'leonard', image: ''}
+    {name: 'sjors', image: 'assets/personen/Leo.PNG'},
+    {name: 'leonard', image: 'assets/personen/Sjors.PNG'},
+    {name: 'martijn', image: 'assets/personen/Martijn.PNG'},
   ]
   constructor() { }
 
